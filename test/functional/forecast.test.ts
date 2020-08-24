817ab1d58d20261782947732e0e5a457455a2b30
Helio Rabelo <heliobrunorabelo@gmail.com>
2020-08-24 00:16:46 -0300

    Configurando Jest

---
+++ test/functional/forecast.test.ts
@@ -0,0 +1,8 @@
+import supertest from 'supertest';
+
+describe('Beach forecast fuctional tests', () => {
+    it('Should return a forecast with just a few times', async() => {
+        //const { body, status} = await supertest(app).get('/forecast');
+        //expect(status).toBe(200);
+    });
+});
