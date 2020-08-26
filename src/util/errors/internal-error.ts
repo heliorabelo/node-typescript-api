307c325de11929bc7873676358b5d6051966dcec
Helio Rabelo <heliobrunorabelo@gmail.com>
2020-08-26 00:03:23 -0300

    Tratamento de erros com typescript e axios

---
+++ src/util/errors/internal-error.ts
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+
+export class InternalError extends Error {
+    constructor(
+        public message: string,
+        protected code: number = 500,
+        protected description?: string
+    ) {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
