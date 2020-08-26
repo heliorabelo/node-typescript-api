1f003214f7e3d798c2cf48e9a082e73328ca43a3
Helio Rabelo <heliobrunorabelo@gmail.com>
2020-08-26 00:47:13 -0300

    Request client com typescript generics

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosStatic } from 'axios';
 import { InternalError } from '@src/util/errors/internal-error';
 import config, { IConfig } from 'config';
+import * as HTTPUtil from '@src/util/request';
 
 export interface StormGlassPointSource {
     [key: string]: number;
@@ -55,7 +55,7 @@ export class StormGlass {
         'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
     readonly stormGlassAPISource = 'noaa';
 
-    constructor(protected request: AxiosStatic = axios) { }
+    constructor(protected request = new HTTPUtil.Request()) { }
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
         try {
@@ -69,7 +69,7 @@ export class StormGlass {
             );
             return this.normalizeResponse(response.data);
         } catch (err) {
-            if (err.response && err.response.status) {
+            if (HTTPUtil.Request.isRequestError(err)) {
                 throw new StormGlassResponseError(`Error: ${JSON.stringify(err.response.data)} Code: ${err.response.status}`);
             }
             throw new ClientRequestError(err.message);
